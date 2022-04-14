81d3080c86686fa0ce7667f3af53274b1b1a1c54
akram56789 <akramhossain56789@gmail>
2022-04-14 15:31:32 +0600

    login-form-done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Login from './components/Login/Login';
 import Shop from './components/Shop/Shop';
+import SignUp from './components/SignUp/SignUp';
 import Orders from './Orders/Orders';
 import About from './utilities/Inventory/About/About';
 import Inventory from './utilities/Inventory/Inventory';
@@ -13,11 +15,14 @@ function App() {
     <div>
       <Header></Header>
    <Routes>
-    
+  
+    <Route path='/' element={<Shop></Shop>}></Route>
     <Route path='/shop' element={<Shop></Shop>}></Route>
     <Route path='/orders' element={<Orders></Orders>}></Route>
     <Route path='/inventory' element={<Inventory></Inventory>}></Route>
     <Route path='/about' element={<About></About>}></Route>
+    <Route path='/login' element={<Login></Login>}></Route>
+    <Route path='/signup' element={<SignUp></SignUp>}></Route>
    </Routes>
 
 
