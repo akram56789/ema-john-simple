a932e0729d2e038643bc8b0ab4ddd19cd15357e3
akram56789 <akramhossain56789@gmail>
2022-04-15 14:57:05 +0600

    firebase-diploy-hosting

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Shipment = () => {
+    const [user] = useAuthState(auth);
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [address, setAddress] = useState('')
@@ -11,9 +14,6 @@ const Shipment = () => {
     const handleNameBlur = event => {
         setName(event.target.value);
 
-    }
-    const handleEmailBlur = event =>{
-        setEmail(event.target.value)
     }
 
     const handleAddressBlur = event => {
@@ -29,12 +29,14 @@ const Shipment = () => {
 
     const handleCreateUser = event => {
         event.preventDefault();
+        const shipping = {name, email, address, phone};
+        console.log(shipping);
 
     }
     return (
         <div className='form-container'>
             <div>
-                <h2 className='form-title'>Shipping Information</h2>
+                <h2 className='form-title'>Your Shipping Info</h2>
                 <form onSubmit={handleCreateUser} >
                     <div className="input-group">
                         <label htmlFor="name">Your Name</label>
@@ -42,14 +44,14 @@ const Shipment = () => {
                     </div>
                     <div className="input-group">
                         <label htmlFor="name"> Your Email</label>
-                        <input onBlur={handleEmailBlur} type="email" name='name' required />
+                        <input  value={user?.email} readOnly type="email" name='name' required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="address">Address</label>
                         <input onBlur={handleAddressBlur} type="text" name='address' required />
                     </div>
                     <div className="input-group">
-                        <label htmlFor="phone-number"> Phone Number</label>
+                        <label htmlFor="phone"> Phone Number</label>
                         <input onBlur={handlePhoneNumberBlur} type=" number" name='phone' required />
                     </div>
                     <p style={{ color: "red" }}>
